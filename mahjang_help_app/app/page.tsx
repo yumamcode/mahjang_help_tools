05925579bd9f7426c2239fb8077f00dc6f7ff97a
= <=>
2024-06-08 01:53:16 +0900

    APIのインストール

--- mahjang_help_app/app/page.tsx
+++ mahjang_help_app/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import xiangtingNum from "@/src/mahjang";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <div>シャン点数は{xiangtingNum}です。</div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
