a0b308db053b78b044ac8dcccb70ef7eb10e72c7
= <=>
2024-06-21 21:20:43 +0900

    赤ドラ枚数が閉じると消える不具合修正

--- mahjang_help_app/components/AkaDorasInput.jsx
+++ mahjang_help_app/components/AkaDorasInput.jsx
@@ -1,12 +1,16 @@
 import { Box, Input ,NumberInput,NumberInputField,NumberInputStepper,NumberIncrementStepper,NumberDecrementStepper} from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const AkaDorasInput = ({akaDoras,setAkaDoras}) =>{
+
+  console.log(akaDoras);
+
   return (
     <>
       <Box className="flex justify-center py-3">
         赤ドラ枚数入力
       </Box>
-      <NumberInput className="mx-auto w-20" defaultValue={0} min={0} max={3} onChange={(v)=>setAkaDoras(v)}>
+      <NumberInput className="mx-auto w-20"  value={akaDoras} min={0} max={3} onChange={(v)=>setAkaDoras(v)}>
         <NumberInputField />
         <NumberInputStepper>
           <NumberIncrementStepper />
--- mahjang_help_app/pages/calculatePoints.jsx
+++ mahjang_help_app/pages/calculatePoints.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import HandInput from '../components/HandInput';
 import MeldInput from '../components/MeldInput';
 import KanInput from '../components/KanInput';
