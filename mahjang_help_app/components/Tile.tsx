982770ca15f1b805339710b6bd7efaf8861d5059
= <=>
2024-07-09 01:18:53 +0900

    Tile.tsx sizeプロパティ追加 onClickプロパティをオプションに変更

--- mahjang_help_app/components/Tile.tsx
+++ mahjang_help_app/components/Tile.tsx
@@ -6,22 +6,33 @@ const Tile = ({
   tile,
   className,
   onClick,
+  size,
 }: {
   tile: string;
   className?: string;
-  onClick: Function;
-}) => (
-  <Provider>
-    <Img
-      src={`/haiImg/${tile}.jpg`}
-      alt={tile}
-      className={className}
-      width="40px"
-      height="auto"
-      onClick={() => onClick(tile)}
-      style={{ cursor: "pointer" }}
-    />
-  </Provider>
-);
+  onClick?: Function;
+  size?: "xs" | "s" | "m" | "l" | "xl";
+}) => {
+  const TILE_SIZE_AND_WIDTH_MAP = {
+    xs: "20px",
+    s: "30px",
+    m: "40px",
+    l: "50px",
+    xl: "60px",
+  };
+  return (
+    <Provider>
+      <Img
+        src={`/haiImg/${tile}.jpg`}
+        alt={tile}
+        className={className}
+        width={size ? TILE_SIZE_AND_WIDTH_MAP[size] : "40px"}
+        height="auto"
+        onClick={() => onClick && onClick(tile)}
+        style={{ cursor: "pointer" }}
+      />
+    </Provider>
+  );
+};
 
 export { Tile };
