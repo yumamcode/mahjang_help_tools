add11f7ee1e40808c944132bbe54610d535d766b
= <=>
2024-07-16 04:23:16 +0900

    getMaxHandLength修正

--- mahjang_help_app/src/getMaxHandLength.ts
+++ mahjang_help_app/src/getMaxHandLength.ts
@@ -1,7 +1,14 @@
 import { MAX_HAND_LENGTH_WITH_MELDS_AND_KANS } from "./Constant";
 
-const getMaxHandLength = (meldsAndKansLength: number): number | undefined => {
+const getMaxHandLengthProcesser = (
+  meldsAndKansLength: number
+): number | undefined => {
   return MAX_HAND_LENGTH_WITH_MELDS_AND_KANS.get(meldsAndKansLength);
+};
+
+const getMaxHandLength = (meldsAndKansLength: number): number => {
+  const maxLength = getMaxHandLengthProcesser(meldsAndKansLength);
+  return maxLength ? maxLength : 0;
 };
 
 export { getMaxHandLength };
