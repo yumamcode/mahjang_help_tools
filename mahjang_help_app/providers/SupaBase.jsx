f28d0360fa502f75451eacaaa0ff836ee136238b
= <=>
2024-07-15 20:43:14 +0900

    supabase アクセスキー ハードコーディング

--- mahjang_help_app/providers/SupaBase.jsx
+++ mahjang_help_app/providers/SupaBase.jsx
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://uibjdzmjzylsfaqgppfu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpYmpkem1qenlsc2ZhcWdwcGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMjQ4NzQsImV4cCI6MjAzNjYwMDg3NH0.vUBNluFqDHUK4X5BlI5euIFWhxt5rveqLzxlOwbHOPI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
